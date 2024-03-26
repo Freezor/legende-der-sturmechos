447bba51c48cad3942983217fd7ccccf221ff3b3
Oliver <info@oliver-fries.de>
2024-03-26 21:18:09 +0100

    update content

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,4 +1,37 @@
 Hooks.on('ready', () => {
     CONFIG.DND5E.abilityConsumptionTypes['echoPoints'] = 'Echopunkte';
 
+    CONFIG.DND5E.languages.exotic.childrenfrankonisch = 'Frankonisch';
+    CONFIG.DND5E.languages.exotic.childrenazumisch = 'Azumisch';
+    CONFIG.DND5E.languages.exotic.childrendauwatin = 'Dauwatin';
+    CONFIG.DND5E.languages.exotic.childrengohyang = 'Gohyang';
+    CONFIG.DND5E.languages.exotic.childrenhyderian = 'Hyderianisch';
+    CONFIG.DND5E.languages.exotic.childrenkaltling = 'Kaltling';
+    CONFIG.DND5E.languages.exotic.childrenkeltonisch = 'Keltonisch';
+    CONFIG.DND5E.languages.exotic.childrenkryllan = 'Kryllan';
+    CONFIG.DND5E.languages.exotic.childrenlattin = 'Lattin';
+    CONFIG.DND5E.languages.exotic.childrenleonin = 'Leonin';
+    CONFIG.DND5E.languages.exotic.childrenlughalsar = 'Lughalsar';
+    CONFIG.DND5E.languages.exotic.childrenmonoisch = 'Monoisch';
+    CONFIG.DND5E.languages.exotic.childrennorse = 'Norse';
+    CONFIG.DND5E.languages.exotic.childrenphabaic = 'Pharabäisch';
+    CONFIG.DND5E.languages.exotic.childrensylvantique = 'Sylvantique';
+    CONFIG.DND5E.languages.exotic.childrenursin = 'Ursinisch';
+    CONFIG.DND5E.languages.exotic.childrenwalin = 'Walin';
+
+    CONFIG.DND5E.toolIds.legionary = "Compendium.legende-der-sturmechos.lds-items.Item.kiSz0EuiCz5o9Wfz";
+    CONFIG.DND5E.toolIds.stonemason = 'Compendium.legende-der-sturmechos.lds-items.Item.AZbgMv1WXPmx0mi0';
+    CONFIG.DND5E.toolIds.documentation = 'Compendium.legende-der-sturmechos.lds-items.Item.wH09aZs8SwFMZ8x1';
+    CONFIG.DND5E.toolIds.archeology = 'Compendium.legende-der-sturmechos.lds-items.Item.2dse7yw2WTbLwvOA';
+    CONFIG.DND5E.toolIds.trapper = 'Compendium.legende-der-sturmechos.lds-items.Item.5nwFh5L3WJAjm5oh';
+    CONFIG.DND5E.toolIds.fisher = 'Compendium.legende-der-sturmechos.lds-items.Item.fFgVjS9dkA6ypdrT';
+    CONFIG.DND5E.toolIds.lumberjack = 'Compendium.legende-der-sturmechos.lds-items.Item.b3WMc1gj2ERdLamJ';
+    CONFIG.DND5E.toolIds.ropemaker = 'Compendium.legende-der-sturmechos.lds-items.Item.54ey5rLZpAKvwmmX';
+    CONFIG.DND5E.toolIds.healer = 'Compendium.legende-der-sturmechos.lds-items.Item.KUVJHrnfDlnm94kv';
+
+
+    CONFIG.DND5E.weaponIds.chepesh = 'Compendium.legende-der-sturmechos.lds-items.Item.FUFKAnfSYsNiSYnR';
+    CONFIG.DND5E.weaponIds.pilum = 'Compendium.legende-der-sturmechos.lds-items.Item.TF9CmdwiTb5WwhKY';
+    CONFIG.DND5E.weaponIds.hiddenblade = 'Compendium.legende-der-sturmechos.lds-items.Item.ddrJ1dVKHzVEgQiM';
+
 });
